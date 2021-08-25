326284a127a92c7e5d696d98226c602217eef2b2
Pea <pea@misfist.com>
2021-08-25 15:58:12 -0400

    Update query block

--- src/App/Blocks/src/eventQuery/edit.js
+++ src/App/Blocks/src/eventQuery/edit.js
@@ -2,6 +2,14 @@
  * WordPress dependencies
  */
  import { 
+	InnerBlocks, 
+	InspectorControls, 
+	useBlockProps,
+	store as blockEditorStore
+} from '@wordpress/block-editor';
+ import { 
+	CustomSelectControl,
+	FormToggle,
 	Panel, 
 	PanelBody, 
 	PanelRow,
@@ -11,25 +19,32 @@
 	Spinner
 } from '@wordpress/components';
 import { 
+	useEffect,
+	useState, 
+	useMemo
+} from '@wordpress/element';
+import { 
+	useDispatch,
+	useSelect
+} from '@wordpress/data';
+import { useInstanceId } from '@wordpress/compose';
+import { 
 	useEntityProp,
 	store as coreStore
 } from '@wordpress/core-data';
 import { 
-	useState, 
-	useMemo
-} from '@wordpress/element';
-import { useSelect } from '@wordpress/data';
-import { 
 	__experimentalGetSettings,
 	 dateI18n 
 } from '@wordpress/date';
-import { 
-	InnerBlocks, 
-	InspectorControls, 
-	useBlockProps
-} from '@wordpress/block-editor';
-import { more } from '@wordpress/icons';
 import { __, sprintf } from '@wordpress/i18n';
+
+const BLOCK_TEMPLATE = [
+	[ 'core/post-featured-image', {}, [] ],
+	[ 'core/post-title', {}, [] ],
+	[ 'wp-action-network-events/event-date', {}, [] ],
+	[ 'wp-action-network-events/event-time', {}, [] ],
+	[ 'wp-action-network-events/event-location', {}, [] ],
+];
 
 const MAX_ITEMS = 24;
 
@@ -43,57 +58,79 @@ const Edit = ( props ) => {
 	} = props;
 
 	const{ 
+		queryId,
 		taxonomy, 
-		eventTagId, 
 		postType, 
+		eventTags, 
 		perPage,
-		orderBy,
-		metaKey,
-		query
+		orderby,
+		query,
+		dateFormat,
+		timeFormat,
+		display: {
+			tagName,
+			showTags,
+			showFeaturedImage,
+			showTitle,
+			showDate,
+			showTime,
+			showLocation
+		}
 	} = attributes;
+
+	// const {
+	// 	tagName,
+	// 	showTags,
+	// 	showFeaturedImage,
+	// 	showTitle,
+	// 	showDate,
+	// 	showTime,
+	// 	showLocation
+	// } = display;
+
+	const instanceId = useInstanceId( Edit );
 
 	const [ siteDateFormat ] = useEntityProp( 'root', 'site', 'date_format' );
 	const [ siteTimeFormat ] = useEntityProp( 'root', 'site', 'time_format' );
 	const settings = __experimentalGetSettings();
+	const resolvedDateFormat = dateFormat || siteDateFormat || settings.formats.date;
+	const resolvedTimeFormat = timeFormat || siteTimeFormat  || settings.formats.date;
 
-	console.log( siteDateFormat, siteTimeFormat, settings );
+	const { __unstableMarkNextChangeAsNotPersistent } = useDispatch(
+		blockEditorStore
+	);
 
-	// const getStartDate = ( postID ) => {
-	// 	const [ meta ] = useEntityProp( 'postType', postType, 'meta', postId, true );
-	// 	const date = meta[ '_start_date' ];
-	// 	return date;
-	// }
+	const posts = useSelect(
+		( select ) => {
+			console.log( query );
 
-	// // To know if the current time format is a 12 hour time, look for "a".
-	// // Also make sure this "a" is not escaped by a "/".
-	// const is12Hour = /a(?!\\)/i.test(
-	// 	settings.formats.time
-	// 		.toLowerCase() // Test only for the lower case "a".
-	// 		.replace( /\\\\/g, '' ) // Replace "//" with empty strings.
-	// 		.split( '' )
-	// 		.reverse()
-	// 		.join( '' ) // Reverse the string and test for "a" not followed by a slash.
-	// );
-	// const formatOptions = Object.values( settings.formats ).map(
-	// 	( formatOption ) => ( {
-	// 		key: formatOption,
-	// 		name: dateI18n( formatOption, date ),
-	// 	} )
-	// );
-	// const resolvedFormat = format || siteDateFormat || settings.formats.date;
+			return select( 'core' ).getEntityRecords( 'postType', postType, query )
+		},
+		[ query ]
+	);
+
+	const setTerms = ( value ) => {
+		setAttributes( {
+			eventTags: value
+		} );
+	}
+
+	const setPerPage = ( value ) => {
+		setAttributes( {
+			perPage: value
+		} );
+	}
+
+	const setOrderBy = ( value ) => {
+		setAttributes( {
+			orderby: value
+		} );
+	}
 
 	const TermSelector = () => {
 		const terms = useSelect( ( select ) => {
 			return select( 'core' ).getEntityRecords( 'taxonomy', taxonomy );
 		}, [] );
-
-		const setTerm = ( term ) => {
-			setAttributes( {
-				eventTagId: term
-			} );
-
-			setQuery( 'eventTagId', term );
-		}
 
 		if( !terms || !terms.length ) {
 			return <Spinner />;
@@ -105,29 +142,21 @@ const Edit = ( props ) => {
 			<>
 				<SelectControl
 					label={ __( 'Tag', 'wp-action-network-events' ) }
-					options={ [ { value: "", label: __( 'Select a Tag', 'wp-action-network-events' ) }, ...options ] || [ { value: "", label: __( 'Loading...', 'wp-action-network-events' ) } ] }
-					onChange={ setTerm }
-					value={ eventTagId }
+					options={ [ { value: "", label: __( 'Select a Tag', 'wp-action-network-events' ) }, ...options ] }
+					onChange={ setTerms }
+					value={ eventTags }
 				/>
 			</>
 		);
 	};
 
-	const PerPostSelector = () => {
-
-		const setValue = ( value ) => {
-			setAttributes( {
-				perPage: value
-			} );
-			setQuery( 'perPage', value );
-		}
-
+	const PerPageSelector = () => {
 		return (
 			<RangeControl
 				key="query-controls-range-control"
 				label={ __( 'Number of Posts', 'wp-action-network-events' ) }
 				value={ perPage }
-				onChange={ setValue }
+				onChange={ setPerPage }
 				min={ 1 }
 				max={ MAX_ITEMS }
 			/>
@@ -138,11 +167,11 @@ const Edit = ( props ) => {
 
 		const options = [
 			{
-				value: "meta_value/desc",
+				value: "start/desc",
 				label: __( 'Soonest to Latest', 'wp-action-network-events' )
 			},
 			{
-				value: "meta_value/asc",
+				value: "start/asc",
 				label: __( 'Latest to Soonest', 'wp-action-network-events' )
 			},
 			{
@@ -150,18 +179,10 @@ const Edit = ( props ) => {
 				label: __( 'A → Z', 'wp-action-network-events' )
 			},
 			{
-				value: "title",
+				value: "title/desc",
 				label: __( 'Z → A', 'wp-action-network-events' )
 			}
 		];
-
-		const setOrderBy = ( value ) => {
-			setAttributes( {
-				orderBy: value
-			} );
-
-			setQuery( 'orderBy', value );
-		}
 
 		if( !options || !options.length ) {
 			return <Spinner />;
@@ -173,13 +194,68 @@ const Edit = ( props ) => {
 					label={ __( 'Order By', 'wp-action-network-events' ) }
 					options={ options }
 					onChange={ setOrderBy }
-					value={ orderBy }
+					value={ orderby }
+				/>
+			</>
+		);
+	};
+
+	const DateFormatSelector = () => {
+		const date = new Date();
+		const options = Object.values( settings.formats ).map(
+			( formatOption ) => ( {
+				key: formatOption,
+				name: dateI18n( formatOption, date ),
+			} )
+		);
+		
+		return (
+			<>
+				<CustomSelectControl
+					label={ __( 'Date Format', 'wp-action-network-events' ) }
+					options={ options }
+					onChange={ ( { selectedItem } ) =>
+						setAttributes( {
+							dateFormat: selectedItem.key,
+						} )
+					}
+					value={ options.find(
+						( option ) => option.key === resolvedDateFormat
+					) }
+				/>
+			</>
+		);
+	};
+
+	const TimeFormatSelector = () => {
+		const date = new Date();
+		const options = Object.values( settings.formats ).map(
+			( formatOption ) => ( {
+				key: formatOption,
+				name: dateI18n( formatOption, date ),
+			} )
+		);
+
+		return (
+			<>
+				<CustomSelectControl
+					label={ __( 'Time Format', 'wp-action-network-events' ) }
+					options={ options }
+					onChange={ ( { selectedItem } ) =>
+						setAttributes( {
+							timeFormat: selectedItem.key,
+						} )
+					}
+					value={ options.find(
+						( option ) => option.key === resolvedTimeFormat
+					) }
 				/>
 			</>
 		);
 	};
 
 	const SettingsPanel = () => (
+		<>
 		<PanelBody title={ __( 'Query Options', 'wp-action-network-events' ) } initialOpen={ true }>
 			<PanelRow>
 				<TermSelector />
@@ -188,59 +264,33 @@ const Edit = ( props ) => {
 				<OrderSelector />
 			</PanelRow>
 			<PanelRow>
-				<PerPostSelector />
+				<PerPageSelector />
 			</PanelRow>
 		</PanelBody>
+		<PanelBody title={ __( 'Display Options', 'wp-action-network-events' ) } initialOpen={ true }>
+			<PanelRow>
+				<DateFormatSelector />
+			</PanelRow>
+			<PanelRow>
+				<TimeFormatSelector />
+			</PanelRow>
+		</PanelBody>
+		</>
 	);
 
-	const setQuery = ( prop, value ) => {
-		let _query = query;
+	const Posts = () => {
 
-		switch( prop ) {
-			case 'perPage' :
-
-				_query = { ...query, per_page: parseInt( value ) }
-
-				break;
-			case 'orderBy' :
-				const _ordering = value.split( '/' );
-				_query = { ..._query, orderby: _ordering[0], order: _ordering[1] }
-
-				if( 'meta_value' === _query.orderby ) {
-					_query = { ..._query, meta_key: metaKey }
-				} else {
-					_query = { ..._query, meta_key: null }
-				}
-
-				break;
-			case 'eventTagId' :
-				console.log( 'eventTagId', value );
-
-				_query = { ..._query, "event-tags": [parseInt( value )] }
-
-				break;
+		if( !posts ) {
+			return <Spinner />
 		}
 
-		setAttributes( {
-			query: _query
-		} );
-	}
-
-	const Posts = () => {
-		const posts = useSelect( ( select ) => {
-			const eventsQuery = {};
-
-			console.log( query );
-
-			return select( 'core' ).getEntityRecords( 'postType', postType, eventsQuery );
-		} );
-
-		if( !posts || !posts.length ) {
-			return <Spinner />
+		if( !posts.length ) {
+			return ':{'
 		}
 
 		return (
 			<>
+				{ console.log( query ) }
 				{ posts.map( post => {
 					return (
 						<Post { ...post } key={post.id} />
@@ -255,22 +305,70 @@ const Edit = ( props ) => {
 			<article className={`${post.type}`}>
 				<h2 className="event__title"><a link={ post.link } rel="bookmark" dangerouslySetInnerHTML={{ __html: post?.title?.rendered }} /></h2>
 				<div className="event__date">
-					<time dateTime={ post.meta?.["_start_date"] }>{ post.meta?.["_start_date"] }</time>
+					<time dateTime={ post.meta?.["_start_date"] }>{ dateI18n( dateFormat, post.meta?.["_start_date"] ) }</time>
 				</div>
 				<div className="event__time">
-					<time dateTime={ post.meta?.["_start_date"] }>{ post.meta?.["_start_date"] }</time>
+					<time dateTime={ post.meta?.["_start_date"] }>{ dateI18n( timeFormat, post.meta?.["_start_date"] ) }</time>
 				</div>
 				<div className="event__location" dangerouslySetInnerHTML={{ __html: post.meta?.["_location_venue"] }}></div>
 			</article>
 		)
 	}
 
-
-	// if( posts ) {
-	// 	console.log( posts );
-	// }
+	const NoPosts = () => {
+		return (
+			<div className="no-posts">
+				No posts
+			</div>
+		)
+	}
 
 	const blockProps = useBlockProps();
+
+	const updateQuery = () => {
+		let _query = query;
+
+		const _ordering = orderby.split( '/' );
+		_query = { 
+			..._query, 
+			per_page: parseInt( perPage ),
+			order: _ordering[1],
+			orderby: _ordering[0],
+			"event-tags": eventTags ? [ parseInt( eventTags ) ] : [],
+		}
+
+		setAttributes( { 
+			query: { 
+				...query, 
+				..._query 
+			} 
+		} );
+	}
+
+	useEffect( () => {
+        updateQuery();
+    }, [ eventTags, perPage, orderby ] );
+
+	useEffect( () => {
+		if ( ! queryId ) {
+			__unstableMarkNextChangeAsNotPersistent();
+			setAttributes( { 
+				queryId: instanceId 
+			} );
+		}
+		if ( ! dateFormat ) {
+			__unstableMarkNextChangeAsNotPersistent();
+			setAttributes( { 
+				dateFormat: resolvedDateFormat
+			} );
+		}
+		if ( ! timeFormat ) {
+			__unstableMarkNextChangeAsNotPersistent();
+			setAttributes( { 
+				timeFormat: resolvedTimeFormat
+			} );
+		}
+	}, [ queryId, instanceId, dateFormat, timeFormat ] );
 
 	return (
 		<>
@@ -279,7 +377,6 @@ const Edit = ( props ) => {
 		</InspectorControls>
 
 		<div { ...blockProps }>
-			Posts
 			<Posts />
 		</div>
 		</>
