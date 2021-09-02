6991500489b396703341e9a5d567fd9315ab533c
Pea <pea@misfist.com>
2021-09-01 20:17:56 -0400

    Wire up admin ajax sync

--- assets/public/js/backend.js
+++ assets/public/js/backend.js
@@ -1,78 +1,40 @@
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./assets/src/sass/backend.scss":
-/*!**************************************!*\
-  !*** ./assets/src/sass/backend.scss ***!
-  \**************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-__webpack_require__.r(__webpack_exports__);
-// extracted by mini-css-extract-plugin
-
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/************************************************************************/
 var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
-(() => {
 /*!**********************************!*\
   !*** ./assets/src/js/backend.js ***!
   \**********************************/
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _sass_backend_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../sass/backend.scss */ "./assets/src/sass/backend.scss");
-/**
- * SASS
- */
+document.addEventListener("DOMContentLoaded", function () {
+  var buttonEl = document.getElementById('wp-action-network-events-sync-button');
+  var nonce = document.getElementById('wp_action_network_events_sync_nonce');
+  var data = wpANEData;
 
-/**
- * JavaScript
- */
+  var onClick = function onClick(event) {
+    event.preventDefault();
+    sendRequest(data);
+  };
 
-/**
- * Add here your JavasScript code
- */
-})();
+  var sendRequest = function sendRequest(props) {
+    var params = {
+      action: data.action
+    };
+    var query = new URLSearchParams(params).toString();
+    fetch(data.ajax_url, {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: new Headers({
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }),
+      body: query
+    }).then(function (response) {
+      return response.json();
+    }).then(function (data) {
+      return console.log(data);
+    });
+  };
 
+  if (buttonEl && data && nonce) {
+    buttonEl.addEventListener('click', onClick);
+  }
+});
 /******/ })()
 ;
