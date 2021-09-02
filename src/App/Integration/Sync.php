950f3fe790518b0145f155c9aa6cb287f9bff3ce
Pea <pea@misfist.com>
2021-09-01 20:16:56 -0400

    Stub out sync class

---
+++ src/App/Integration/Sync.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * The plugin options.
+ *
+ * @link       https://debtcollective.org
+ * @since      1.0.0
+ *
+ * @package    Wp_Action_Network_Events
+ * @subpackage Wp_Action_Network_Events/admin
+ */
+namespace WpActionNetworkEvents\App\Integration;
+
+use WpActionNetworkEvents\Common\Abstracts\Base;
+use WpActionNetworkEvents\App\Integration\GetEvents;
+
+/**
+ * Plugin Options
+ *
+ *
+ * @package    Wp_Action_Network_Events
+ * @subpackage Wp_Action_Network_Events/admin
+ * @author     Debt Collective <pea@misfist.com>
+ */
+class Sync extends Base {
+
+/**
+	 * Constructor.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __construct( $version, $plugin_name ) {
+		parent::__construct( $version, $plugin_name );
+		$this->init();
+	}
+
+	/**
+	 * Initialize the class.
+	 *
+	 * @since 0.1.0
+	 */
+	public function init() {
+		/**
+		 * This general class is always being instantiated as requested in the Bootstrap class
+		 *
+		 * @see Bootstrap::__construct
+		 *
+		 */
+
+	}
+
+	function getData() {}
+
+	function parseData() {}
+
+	function updateRecords() {}
+
+	function updateRecord() {}
+
+	function recordExists() {}
+
+	function hasChanged() {}
+}
