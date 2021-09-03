2e5e74ee5fd24527fd878f073a238d7462f37cd7
Pea <pea@misfist.com>
2021-09-03 01:32:14 -0400

    Move sync actions out of GetData abstract class

--- src/Common/Abstracts/GetData.php
+++ src/Common/Abstracts/GetData.php
@@ -91,10 +91,10 @@ abstract class GetData {
 		$this->api_key = $options['api_key'];
 		$this->base_url = $options['base_url'];
 
-		\add_action( 'admin_enqueue_scripts', 							[ $this, 'enqueueScripts' ] );
+		// \add_action( 'admin_enqueue_scripts', 							[ $this, 'enqueueScripts' ] );
 
-		\add_action( 'wp_ajax_' . Options::SYNC_ACTION_NAME, 			[ $this, 'fetchData' ] );
-		\add_action( 'wp_ajax_nopriv_' . Options::SYNC_ACTION_NAME, 	[ $this, 'fetchData' ] );
+		// \add_action( 'wp_ajax_' . Options::SYNC_ACTION_NAME, 			[ $this, 'sendData' ] );
+		// \add_action( 'wp_ajax_nopriv_' . Options::SYNC_ACTION_NAME, 	[ $this, 'sendData' ] );
 	}
 
 	/**
@@ -113,7 +113,7 @@ abstract class GetData {
 	 *
 	 * @return mixed (array|WP_Error) The response or WP_Error on failure.
 	 */
-	public function fetchData() {
+	public function fetchData( $page = 1 ) {
 
 		$endpoint = \esc_url( $this->base_url . $this->endpoint );
 
@@ -122,18 +122,71 @@ abstract class GetData {
 				'Content-Type' 			=> 'application/json',
 				'OSDI-API-Token' 		=> $this->api_key,
 			],
-			'timeout'     => 100,
-			'redirection' => 5,
+			'timeout'     				=> 100,
+			'redirection' 				=> 5,
+			'body'						=> [
+				'page'					=> $page
+			]	
 		];
-
-		$options = \wp_parse_args( $args, $options );
 
 		$response = \wp_remote_get( $endpoint, $options );
 
-
-		\wp_send_json( $response );
-		\wp_die();
+		return $response;
 	}
+
+	/**
+	 * Get the response body
+	 * 
+	 * @see https://developer.wordpress.org/reference/functions/wp_remote_retrieve_body/
+	 *
+	 * @return mixed (array|WP_Error) The response or WP_Error on failure.
+	 */
+	public function getResponseBody( $page = 1 ) {
+		$response = $this->fetchData( $page );
+		if( empty( $response ) && !is_wp_error( $response ) ) {
+			$response_code = wp_remote_retrieve_response_code( $response );
+			return new WP_Error( 'response-error', __( "There was an error in the response. (Code $response_code)", "wp-action-network-events" ) );
+		}
+		$body = wp_remote_retrieve_body( $response );
+		return json_decode( $body );
+	}
+
+	/**
+	 * Get the response code
+	 * 
+	 * @see https://developer.wordpress.org/reference/functions/wp_remote_retrieve_response_code/
+	 *
+	 * @return mixed (array|WP_Error) The response or WP_Error on failure.
+	 */
+	public function getResponseCode( $page = 1 ) {
+		$response = $this->fetchData( $page );
+		return wp_remote_retrieve_response_code( $response );
+	}
+
+	/**
+	 * Get the response pages
+	 *
+	 * @return mixed (array|WP_Error) The response or WP_Error on failure.
+	 */
+	public function getResponsePages() {
+		$response = $this->getResponseBody();
+		if( empty( $response ) && !is_wp_error( $response ) ) {
+			return new WP_Error( 'no-response-body', __( "No response body was returned", "wp-action-network-events" ) );
+		}
+		return $response->total_pages;
+	}
+
+	/**
+	 * Send Data to Ajax
+	 *
+	 * @return void
+	 */
+	// public function sendData() {
+	// 	$data = $this->fetchData();
+
+	// 	\wp_send_json( $data );
+	// 	\wp_die();
+	// }
 
 	/**
 	 * Get the data at a given endpoint
