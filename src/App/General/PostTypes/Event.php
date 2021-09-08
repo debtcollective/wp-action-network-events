61ae31a0c1423b19e54bbe8f43be0dbb1c38fe2d
Pea <pea@misfist.com>
2021-09-08 18:57:50 -0400

    Add status and field constants to event class

--- src/App/General/PostTypes/Event.php
+++ src/App/General/PostTypes/Event.php
@@ -17,6 +17,18 @@ use WpActionNetworkEvents\Common\Abstracts\PostType;
 class Event extends PostType {
 
 	/**
+	 * Status map to WP post_status
+	 * 
+	 * @since 1.0.0
+	 * @var      array    STATUSES
+	 */
+	const STATUSES = [
+		'confirmed' 		=> 'publish',
+		'tentative' 		=> 'draft',
+		'cancelled' 		=> 'trash',
+	];
+
+	/**
 	 * Post type data
 	 */
 	public const POST_TYPE = [
@@ -28,6 +40,18 @@ class Event extends PostType {
 		'icon'     		=> 'dashicons-calendar-alt',
 		'taxonomies'	=> [ 'event_type' ],
 		'rest_base'		=> 'events',
+	];
+
+	/**
+	 * Event fields
+	 */
+	public const FIELDS = [
+		'post_title' 			=> 'name',
+		'post_content' 			=> 'description',
+		'post_date'				=> 'created_date',
+		'post_modified'			=> 'modified_date',
+		'post_status'			=> '',
+		'import_id'				=> 'identifiers[0]',
 	];
 
 }
