2315d8f4a53dca7c8677e0f22e047ca94f83327d
Pea <pea@misfist.com>
2022-01-13 17:45:25 -0500

    Update data retrieval abstract class
    Added error handling and pagination
    @todo separate error handling and recursion

--- src/Common/Abstracts/GetData.php
+++ src/Common/Abstracts/GetData.php
@@ -7,6 +7,7 @@
 namespace WpActionNetworkEvents\Common\Abstracts;
 
 use WpActionNetworkEvents\App\Admin\Options;
+use WpActionNetworkEvents\Common\Util\Iterator;
 
 /**
  * The Data class which can be extended by other classes to load in default methods
@@ -17,8 +18,27 @@ use WpActionNetworkEvents\App\Admin\Options;
 abstract class GetData {
 
 	/**
+	 * The unique identifier of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      string    $plugin_name    The string used to uniquely identify this plugin.
+	 */
+	protected $plugin_name;
+
+	/**
+	 * The current version of the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      string    $version    The current version of the plugin.
+	 */
+	protected $version;
+
+	/**
 	 * Base URL
 	 *
+	 * @since    1.0.0
 	 * @var string
 	 */
 	private $base_url;
@@ -51,183 +71,188 @@ abstract class GetData {
 	protected $args;
 
 	/**
-	 * Array of data.
+	 * API Data.
 	 *
 	 * @since    1.0.0
 	 * @access   protected
-	 * @var      array   $data
+	 * @var      object   $data
 	 */
 	protected $data;
 
 	/**
-	 * Array of errors
+	 * Records Per Page
 	 *
 	 * @since    1.0.0
 	 * @access   protected
-	 * @var      array   $errors
+	 * @var int
 	 */
-	protected $errors;
+	protected $per_page;
 
 	/**
-	 * The unique identifier of this plugin.
+	 * Filter By Modification Date
 	 *
 	 * @since    1.0.0
 	 * @access   protected
-	 * @var      string    $plugin_name    The string used to uniquely identify this plugin.
+	 * @var string
 	 */
-	protected $plugin_name;
+	protected $search_filter;
 
 	/**
-	 * The current version of the plugin.
+	 * Current Page Number
 	 *
 	 * @since    1.0.0
 	 * @access   protected
-	 * @var      string    $version    The current version of the plugin.
+	 * @var int
 	 */
-	protected $version;
+	protected $current_page;
+
 
 	/**
-	 * Data constructor.
+	 * Total Number of Records
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var int
+	 */
+	protected $total_records;
+
+	/**
+	 * Total Number of Pages
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var int
+	 */
+	protected $total_pages;
+
+	/**
+	 * Constructor.
 	 *
 	 * @since 1.0.0
 	 */
-	public function __construct( string $endpoint, array $args = [], string $version , string $plugin_name ) {
-		$this->endpoint = $endpoint;
-		$this->args = $args;
-		$this->version = $version;
-		$this->plugin_name = $plugin_name;
-		$options = Options::getOptions();
-		if( $options && $options['api_key'] && $options['base_url'] ) {
-			$this->api_key = $options['api_key'];
-			$this->base_url = $options['base_url'];
-			$this->data = $this->getCollection();
-		} else {
-			$this->handleError( 'Options not set' );
-		}
+	public function __construct( string $version, string $plugin_name, string $endpoint, $args = array() ) {
+		$this->endpoint      = $endpoint;
+		$this->version       = $version;
+		$this->plugin_name   = $plugin_name;
+		$this->search_filter = isset( $args['filter'] ) ? $args['filter'] : null;
+		$this->per_page      = isset( $args['per_page'] ) ? (int) $args['per_page'] : 25;
+		$this->current_page  = 0;
 
+		$options        = Options::getOptions();
+		$this->api_key  = isset( $options['api_key'] ) ? $options['api_key'] : null;
+		$this->base_url = isset( $options['base_url'] ) ? $options['base_url'] : null;
+
+		if ( $this->api_key && $this->base_url ) {
+			$this->getRecords();
+		}
 	}
 
 	/**
-	 * Kick it off
+	 * Make Request
+	 *
+	 * @link https://developer.wordpress.org/reference/functions/wp_remote_get/
+	 *
+	 * @param string $url
+	 * @return mixed object $request || WP_Error
+	 */
+	public function request( $url ) {
+		$options = array(
+			'headers'     => array(
+				'Content-Type'   => 'application/json',
+				'OSDI-API-Token' => $this->api_key,
+			),
+			'timeout'     => 100,
+			'redirection' => 5,
+		);
+
+		$request = \wp_remote_get( $url, $options );
+
+		if ( is_a( $request, '\WP_Error' ) ) {
+			throw new \Exception( \wp_remote_retrieve_response_code( $request ) . ': ' . json_decode( \wp_remote_retrieve_body( $request ), true)['error'] );
+		}
+
+		return $request;
+	}
+
+	/**
+	 * Get Records
 	 *
 	 * @return void
 	 */
-	public function init() {
-		$this->registerEventTypeOptions();
-	}
+	public function getRecords() {
+		$args = array(
+			'per_page' => $this->per_page,
+		);
+		if ( $this->search_filter ) {
+			$args['filter'] = "modified_date gt '{$this->search_filter}'";
+		}
+		$query = http_build_query( $args );
+		$url   = \esc_url( $this->base_url . $this->endpoint ) . "?$query";
 
-	/**
-	 * Get Data
-	 * 
-	 * @see https://developer.wordpress.org/reference/functions/wp_remote_get/
-	 *
-	 * @return mixed (array|WP_Error) The response or WP_Error on failure.
-	 */
-	public function getRequest( $page = 1 ) {
-		$endpoint = \esc_url( $this->base_url . $this->endpoint );
+		try {
+			$request = $this->request( $url );
 
-		$options = [
-			'headers' => [
-				'Content-Type' 			=> 'application/json',
-				'OSDI-API-Token' 		=> $this->api_key,
-			],
-			'timeout'     				=> 100,
-			'redirection' 				=> 5,
-			'body'						=> [
-				'page'					=> $page
-			]	
-		];
+			if ( 200 === \wp_remote_retrieve_response_code( $request ) ) {
+				$response   = json_decode( \wp_remote_retrieve_body( $request ), true );
+				$this->data = $response['_embedded']['osdi:events'];
 
-		$response = \wp_remote_get( $endpoint, $options );
+				while ( $response['total_pages'] > $response['page'] && isset( $response['_links']['next']['href'] ) ) {
+					$url     = $response['_links']['next']['href'];
+					$request = $this->request( $url );
+					error_log( $url );
 
+					if ( 200 === \wp_remote_retrieve_response_code( $request ) ) {
+						$nextResponse = json_decode( \wp_remote_retrieve_body( $request ), true );
+						$data         = $nextResponse['_embedded']['osdi:events'];
+						$this->data   = array_merge( $this->data, $data );
+						$response     = array_merge( $response, $nextResponse );
+					} else {
+						error_log( \wp_remote_retrieve_response_code( $request ) . ': ' . \wp_remote_retrieve_body( $request ) );
+						$response = null;
+					}
+				}
+				error_log( count( $this->data ) );
+			} else {
+				error_log( \wp_remote_retrieve_response_code( $request ) . ': ' . json_decode( \wp_remote_retrieve_body( $request ), true)['error'] );
+				$response = null;
+			}
+		} catch ( \Exception $exception ) {
+			error_log( $exception );
+			$response = null;
+			$data     = null;
+		}
+		// return $data;
 		return $response;
 	}
 
 	/**
-	 * Get Entire Collection
-	 * If multiple pages, get all
+	 * Get Record Data
 	 *
-	 * @return void
+	 * @return mixed object || null
 	 */
-	abstract public function getCollection() : array;
-
-	/**
-	 * Get the response body
-	 * 
-	 * @see https://developer.wordpress.org/reference/functions/wp_remote_retrieve_body/
-	 *
-	 * @return mixed (array|WP_Error) The response or WP_Error on failure.
-	 */
-	public function getResponseBody( $page = 1 ) {
-		$response = $this->getRequest( $page );
-		if( empty( $response ) || !is_wp_error( $response ) ) {
-			return $this->handleError( $response );
-		}
-		$body = wp_remote_retrieve_body( $response );
-		return json_decode( $body );
-	}
-
-	/**
-	 * Get the response code
-	 * 
-	 * @see https://developer.wordpress.org/reference/functions/wp_remote_retrieve_response_code/
-	 *
-	 * @return mixed (array|WP_Error) The response or WP_Error on failure.
-	 */
-	public function getResponseCode( $response ) {
-		return wp_remote_retrieve_response_code( $response );
-	}
-
-	/**
-	 * Get the response pages
-	 *
-	 * @return mixed (array|WP_Error) The response or WP_Error on failure.
-	 */
-	public function getResponsePages() {
-		$response = $this->getResponseBody();
-		if( empty( $response ) || !is_wp_error( $response ) ) {
-			return $this->handleError( $response );
-		}
-		return $response->total_pages;
-	}
-
-	/**
-	 * Get the data at a given endpoint
-	 *
-	 * @param string $endpoint
-	 * @return mixed (array|WP_Error) The response or WP_Error on failure.
-	 */
-	public static function getData() {
+	public function getData() {
 		return $this->data;
 	}
 
 	/**
-	 * Handle Errors
+	 * Handle Request
 	 *
-	 * @return void
+	 * @param object $request
+	 * @return mixed object $response || null;
 	 */
-	function handleError( $exception ) {
-		$this->errors[] = $exception;
-		// throw new \Exception( $exception );
-	}
-
-	/**
-	 * Register Resource Type
-	 * Will be added to the list of types available on Options page
-	 *
-	 * @return void
-	 */
-	public function registerEventTypeOptions() {
-		$new_types = $this->args['types'];
-		$current_types = Options::getEventTypeOptions();
-		if( !array_key_exists( $this->endpoint, $current_types ) ) {
-			Options::registerEventTypeOptions( $this->types );
+	public function handleRequest( $request ) {
+		if ( 200 === \wp_remote_retrieve_response_code( $request ) ) {
+			$response = json_decode( \wp_remote_retrieve_body( $request ), true );
+		} else {
+			error_log( \wp_remote_retrieve_response_code( $request ) . ': ' . \wp_remote_retrieve_body( $request ) );
+			$response = null;
 		}
+		return $response;
 	}
 
 	/**
 	 * Enqueue Scripts
-	 * 
+	 *
 	 * @see https://developer.wordpress.org/reference/hooks/admin_enqueue_scripts/
 	 *
 	 * @return void
@@ -235,11 +260,11 @@ abstract class GetData {
 	public function enqueueScripts() {
 		\wp_register_script( $this->plugin_name, esc_url( WPANE_PLUGIN_URL . 'assets/public/js/backend.js' ), array( 'jquery' ), $this->version, false );
 
-		$localized = [
-			'action'		=> Options::SYNC_ACTION_NAME,
-			'endpoint'		=> $this->endpoint,
-			'ajax_url' 		=> \admin_url( 'admin-ajax.php' )
-		];
+		$localized = array(
+			'action'   => Options::SYNC_ACTION_NAME,
+			'endpoint' => $this->endpoint,
+			'ajax_url' => \admin_url( 'admin-ajax.php' ),
+		);
 
 		\wp_localize_script( $this->plugin_name, 'wpANEData', $localized );
 
