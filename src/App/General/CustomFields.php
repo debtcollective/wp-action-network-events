77072af8a72ea8bc578891ca81d0ee69c4865e65
Pea <pea@misfist.com>
2022-01-20 23:47:44 -0500

    Coding Standards

--- src/App/General/CustomFields.php
+++ src/App/General/CustomFields.php
@@ -24,7 +24,7 @@ class CustomFields extends Base {
 
 	/**
 	 * Metabox Container ID
-	 * 
+	 *
 	 * @since 1.0.0
 	 */
 	public const CONTAINER_ID = 'wp_action_network_fields';
@@ -32,7 +32,7 @@ class CustomFields extends Base {
 	/**
 	 * Custom fields
 	 */
-	public const FIELDS = [
+	public const FIELDS = array(
 		'browser_url',
 		'an_id',
 		'instructions',
@@ -99,18 +99,15 @@ class CustomFields extends Base {
 		 * This general class is always being instantiated as requested in the Bootstrap class
 		 *
 		 * @see Bootstrap::__construct
-		 *
 		 */
-		\add_action( 'init',							[ $this, 'registerPostMeta' ] );
+		\add_action( 'init', array( $this, 'registerPostMeta' ) );
 
 		/**
 		 * Don't hide custom fields meta box
+		 *
 		 * @see https://www.advancedcustomfields.com/resources/acf-settings/
 		 */
 		\add_filter( 'acf/settings/remove_wp_meta_box', '__return_false' );
-
-		// \add_action( 'plugins_loaded', 					[ $this, 'load' ] );
-		// \add_action( 'carbon_fields_register_fields', 	[ $this, 'addFields' ] );
 
 		/**
 		 * API Fields
@@ -128,135 +125,45 @@ class CustomFields extends Base {
 		 * location.venue,
 		 * location.location.latitude,
 		 * location.location.longitude,
-		 * status ["confirmed" "tentative" "cancelled"] 
+		 * status ["confirmed" "tentative" "cancelled"]
 		 * visibility ["public" "private"]
-		 * action_network:event_campaign_id 
+		 * action_network:event_campaign_id
 		 */
 	}
 
 	/**
-	 * Load Fields Library
-	 *
-	 * @return void
-	 */
-	public function load() {
-		\Carbon_Fields\Carbon_Fields::boot();
-	}
-
-	/**
-	 * Add Custom Fields
-	 *
-	 * @since 0.1.0
-	 */
-	public function addFields() {
-
-		$is_read_only = false;
-
-		$default_timezone = get_option( 'timezone_string' );
-
-		$fields = [
-			Field::make( 'text', 'name', __( 'Name', 'wp-action-network-events' ) )
-				->set_visible_in_rest_api( $visible = true )
-				->set_attribute( 'readOnly', $is_read_only ),
-			Field::make( 'rich_text', 'instructions', __( 'Instructions', 'wp-action-network-events' ) )
-				->set_visible_in_rest_api( $visible = true )
-				->set_attribute( 'readOnly', $is_read_only ),
-			Field::make( 'text', 'browser_url', __( 'URL', 'wp-action-network-events' ) )
-				->set_visible_in_rest_api( $visible = true )
-				->set_attribute( 'readOnly', $is_read_only )
-				->set_attribute( 'type', 'url' ),
-			Field::make( 'text', 'start_date', __( 'Start', 'wp-action-network-events' ) )
-				->set_visible_in_rest_api( $visible = true )
-				->set_attribute( 'readOnly', $is_read_only ),
-			Field::make( 'text', 'end_date', __( 'End', 'wp-action-network-events' ) )
-				->set_visible_in_rest_api( $visible = true )
-				->set_attribute( 'readOnly', $is_read_only ),
-			Field::make( 'select', 'time_zone', __( 'Time Zone', 'wp-action-network-events' ) )
-				->add_options( [ $this, 'getTimezones' ] )
-				->set_default_value( $default_timezone )
-				->set_visible_in_rest_api( $visible = true ),
-
-			Field::make( 'separator', 'separator_location', __( 'Location Detail', 'wp-action-network-events' ) ),
-			Field::make( 'text', 'location_venue', __( 'Venue', 'wp-action-network-events' ) )
-				->set_visible_in_rest_api( $visible = true )
-				->set_attribute( 'readOnly', $is_read_only ),
-			Field::make( 'text', 'location_locality', __( 'Locality', 'wp-action-network-events' ) )
-				->set_visible_in_rest_api( $visible = true )
-				->set_attribute( 'readOnly', $is_read_only ),
-			Field::make( 'text', 'location_address', __( 'Address', 'wp-action-network-events' ) )
-				->set_visible_in_rest_api( $visible = true )
-				->set_attribute( 'readOnly', $is_read_only ),
-			Field::make( 'text', 'location_postal_code', __( 'Postal Code', 'wp-action-network-events' ) )
-				->set_visible_in_rest_api( $visible = true )
-				->set_attribute( 'readOnly', $is_read_only ),
-			Field::make( 'text', 'location_region', __( 'Region', 'wp-action-network-events' ) )
-				->set_visible_in_rest_api( $visible = true )
-				->set_attribute( 'readOnly', $is_read_only ),
-			Field::make( 'text', 'location_country', __( 'Country', 'wp-action-network-events' ) )
-				->set_visible_in_rest_api( $visible = true )
-				->set_attribute( 'readOnly', $is_read_only ),
-			Field::make( 'text', 'location_longitude', __( 'Longitude', 'wp-action-network-events' ) )
-				->set_visible_in_rest_api( $visible = true )
-				->set_attribute( 'readOnly', $is_read_only ),
-			Field::make( 'text', 'location_latitude', __( 'Latitude', 'wp-action-network-events' ) )
-				->set_visible_in_rest_api( $visible = true )
-				->set_attribute( 'readOnly', $is_read_only ),
-			Field::make( 'text', 'location_accuracy', __( 'Accuracy', 'wp-action-network-events' ) )
-				->set_visible_in_rest_api( $visible = true )
-				->set_attribute( 'readOnly', $is_read_only ),
-			
-			Field::make( 'separator', 'separator_campaign', __( 'Campaign Details', 'wp-action-network-events' ) ),
-			Field::make( 'text', 'an_campaign_id', __( 'Campaign ID', 'wp-action-network-events' ) )
-				->set_visible_in_rest_api( $visible = true )
-				->set_attribute( 'readOnly', $is_read_only ),
-
-			Field::make( 'separator', 'separator_misc', __( 'Misc Details', 'wp-action-network-events' ) ),
-			Field::make( 'text', 'an_id', __( 'ID', 'wp-action-network-events' ) )
-				->set_visible_in_rest_api( $visible = true )
-				->set_attribute( 'readOnly', $is_read_only ),
-			Field::make( 'text', 'modified_date', __( 'Modified Date', 'wp-action-network-events' ) )
-				->set_visible_in_rest_api( $visible = true )
-				->set_attribute( 'readOnly', $is_read_only ),
-			Field::make( 'text', 'status', __( 'Status', 'wp-action-network-events' ) )
-				->set_visible_in_rest_api( $visible = true )
-				->set_attribute( 'readOnly', $is_read_only ),
-			Field::make( 'text', 'visibility', __( 'Visibility', 'wp-action-network-events' ) )
-				->set_visible_in_rest_api( $visible = true )
-				->set_attribute( 'readOnly', $is_read_only ),
-		];
-
-		Container::make( 
-			'post_meta',
-			self::CONTAINER_ID,
-			__( 'Event Details', 'wp-action-network-events' ) 
-		)
-			->where( 'post_type', '=', Event::POST_TYPE['id'] )
-			->set_context( 'advanced' )
-			->add_fields( $fields );
-	}
-
-	/**
 	 * Register post meta with Rest API
-	 * 
+	 *
 	 * @see https://developer.wordpress.org/reference/functions/register_post_meta/
 	 *
 	 * @return void
 	 */
 	public function registerPostMeta() {
 
-		foreach( self::FIELDS as $field ) {
-			\register_post_meta(
-				Event::POST_TYPE['id'], 
-				$field, [
-					'show_in_rest' 	=> true,
-					'single' 		=> true,
-					'type' 			=> 'string',
-				]
-			);
+		foreach ( self::FIELDS as $field ) {
+			if ( 'hidden' === $field ) {
+				\register_post_meta(
+					Event::POST_TYPE['id'],
+					$field,
+					array(
+						'show_in_rest' => true,
+						'single'       => true,
+						'type'         => 'boolean',
+					)
+				);
+			} else {
+				\register_post_meta(
+					Event::POST_TYPE['id'],
+					$field,
+					array(
+						'show_in_rest' => true,
+						'single'       => true,
+						'type'         => 'string',
+					)
+				);
+			}
 		}
 	}
-
-	public static function getFields() {}
 
 	/**
 	 * Build list of timezones
@@ -265,15 +172,15 @@ class CustomFields extends Base {
 	 */
 	public function getTimezones() {
 		$timezones = \DateTimeZone::listIdentifiers();
-		$array = [];
+		$array     = array();
 
 		$count = count( $timezones );
-		for( $i = 0; $i <= $count; $i++ ) {
-			if( !empty( $timezones[$i] ) ) {
-				$array[$timezones[$i]] = str_replace( '_', ' ', $timezones[$i] );
+		for ( $i = 0; $i <= $count; $i++ ) {
+			if ( ! empty( $timezones[ $i ] ) ) {
+				$array[ $timezones[ $i ] ] = str_replace( '_', ' ', $timezones[ $i ] );
 			}
 		}
-	
+
 		return $array;
 	}
 }
